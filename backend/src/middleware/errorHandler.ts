b3d98c8dd36f8b0d9e7ee88bdb4a098681e0ffa6
furkotikoo <furkanyildiz47@gmail.com>
2024-07-28 11:14:30 +0300

    errorHandler is created

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,6 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { ApiError } from '../types/error/ApiError';
+import { StatusCodes } from 'http-status-codes';
 
-export const errorHandler = (error: Error, req: Request, res: Response) => {
-  console.error(`Error: ${error.message}`);
-  return res.status(500).json({ message: 'Internal server error' });
+export const errorHandler = (
+  err: ApiError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+) => {
+  console.error(`Error: ${err.message}`);
+  const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  res.status(statusCode).send(err.toErrorResponse());
 };
