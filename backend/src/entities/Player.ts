bf13f8cbbde4c805d2de833f234cb7c970da743d
furkotikoo <furkanyildiz47@gmail.com>
2024-07-28 13:11:46 +0300

    toJson method added to Game and Player entities while serializing

--- backend/src/entities/Player.ts
+++ backend/src/entities/Player.ts
@@ -4,21 +4,34 @@ import {
   Column,
   OneToOne,
   JoinColumn,
+  Unique,
 } from 'typeorm';
 import { Game } from './Game';
 
 @Entity('players')
+@Unique('UQ_Player', ['fullName', 'ipAddress', 'game'])
 export class Player {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: false, unique: true })
+  @Column({ nullable: false })
   fullName: string;
 
-  @Column({ nullable: false, unique: true })
+  @Column({ nullable: false })
   ipAddress: string;
 
   @OneToOne(() => Game, (game) => game.gameCode)
   @JoinColumn()
   game: Game;
+
+  toJSON() {
+    const { game, ...player } = this;
+    return {
+      ...player,
+      game: {
+        id: game.id,
+        name: game.gameCode,
+      },
+    };
+  }
 }
