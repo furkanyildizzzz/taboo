f2b7e6262a6c2c41e9faf28c45c88543fd26e639
furkotikoo <furkanyildiz47@gmail.com>
2024-07-30 00:46:34 +0300

    cors is applied

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,21 @@ import gameRoutes from './routes/gameRoutes';
 import playerRoutes from './routes/playerRoutes';
 import { errorHandler } from './middleware/errorHandler';
 import { NotFoundError } from './types/error/NotFoundError';
+import cors from 'cors';
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
+// CORS configuration
+app.use(
+  cors({
+    origin: '*', // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    credentials: true, // Allow cookies and other credentials
+    preflightContinue: false,
+  }),
+);
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -19,8 +29,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-app.use('/game', gameRoutes);
-app.use('/player', playerRoutes);
+app.use('/api/game', gameRoutes);
+app.use('/api/player', playerRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
