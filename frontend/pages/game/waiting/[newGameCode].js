6dd5a90a32b66b88f2bfa0aa0838845b20a47b51
furkotikoo <furkanyildiz47@gmail.com>
2024-07-31 23:12:32 +0300

    join game page created

--- frontend/pages/game/waiting/[newGameCode].js
+++ frontend/pages/game/waiting/[newGameCode].js
@@ -1,6 +1,12 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import {
+  initializeSocket,
+  joinGameRoom,
+  onNewMessage,
+  onNewUser,
+} from '@/services/socketServices';
 
 export async function getStaticPaths() {
   return {
@@ -19,6 +25,30 @@ export async function getStaticProps({ locale }) {
 const NewGame = () => {
   const { query } = useRouter();
   const { newGameCode } = query;
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const socket = initializeSocket();
+
+    joinGameRoom(newGameCode);
+
+    onNewMessage((message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    onNewUser((message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [newGameCode]);
+
+  const handleSendMessage = () => {
+    sendMessage('Hello from frontend!', newGameCode);
+  };
 
   return <div>Game Code {newGameCode}</div>;
 };
